cc50e52b6b1aa5a8e01bab519bfb1d59468be1fc
hokagelegend2023 <136045532+hokagelegend2023@users.noreply.github.com>
2023-08-18 13:57:43 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -162,6 +162,7 @@ cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 rm /etc/nginx/conf.d/vps.conf
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/ssh/nginx.conf"
 wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/ssh/vps.conf"
 /etc/init.d/nginx restart
 
